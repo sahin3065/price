fb3357cc335e7adf80429b00e41f52ccc523c6c9
sahin3065 <sahinalom5161@gmail.com>
2024-03-20 22:16:40 +0600

    responsive navbar using react and tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 
 import './App.css'
+import DaisyNav from './components/DaisyNav/DaisyNav'
+import Navbar from './components/Navbar/Navbar'
 
 function App() {
  
 
   return (
     <>
-     
+      {/* <DaisyNav></DaisyNav> */}
+      <Navbar></Navbar>
       <h1 className='text-7xl bg-rose-600'>Vite + React</h1>
      
     </>
